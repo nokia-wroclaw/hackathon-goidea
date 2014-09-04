afaf63234644cd9393871eb044aa166a5366c3ed
chmurson <chmurson@gmail.com>
2014-09-04 12:48:07 +0200

    grunt

--- static/js/sharedRequireConfig.js
+++ static/js/sharedRequireConfig.js
@@ -4,21 +4,21 @@ define([], function() {
   return {
     baseUrl: 'static/js',
     paths: {
-//      'angular': './libs/angular.min',
-//      'angular-route': './libs/angular-route.min',
-//      'ui-router': './libs/angular-ui-router.min',
-//      'ui-bootstrap': './libs/bootstrap/ui-bootstrap.min',
-//      '_': './libs/lodash',
-//      'jquery': './libs/jquery',
-//      'jquery-ui': './libs/jquery-ui/jquery-ui',
-//      'select2': './libs/select2/select2',
-//      'slimscroll': './libs/slimscroll',
-//      'prettify': './libs/prettify/prettify',
-//      'jquery.bootstrap-duallistbox': './libs/duallistbox/jquery.bootstrap-duallistbox.min',
-//      'angular-duallistbox': './libs/duallistbox/angular-bootstrap-duallistbox.min',
-//      'ng-infinite-scroll': './libs/ng-infinite-scroll',
-//      'DynamicTableModel': './libs/DynamicTableModel',
-//      'xeditable': './libs/angular-xeditable/js/xeditable'
+      //      'angular': './libs/angular.min',
+      //      'angular-route': './libs/angular-route.min',
+      //      'ui-router': './libs/angular-ui-router.min',
+      //      'ui-bootstrap': './libs/bootstrap/ui-bootstrap.min',
+      //      '_': './libs/lodash',
+      //      'jquery': './libs/jquery',
+      //      'jquery-ui': './libs/jquery-ui/jquery-ui',
+      //      'select2': './libs/select2/select2',
+      //      'slimscroll': './libs/slimscroll',
+      //      'prettify': './libs/prettify/prettify',
+      //      'jquery.bootstrap-duallistbox': './libs/duallistbox/jquery.bootstrap-duallistbox.min',
+      //      'angular-duallistbox': './libs/duallistbox/angular-bootstrap-duallistbox.min',
+      //      'ng-infinite-scroll': './libs/ng-infinite-scroll',
+      //      'DynamicTableModel': './libs/DynamicTableModel',
+      //      'xeditable': './libs/angular-xeditable/js/xeditable'
     },
     map: {
       '*': {
@@ -27,28 +27,28 @@ define([], function() {
       }
     },
     shim: {
-//      'angular': {
-//        'deps': ['jquery'],
-//        'exports': 'angular'
-//      },
-//      'prettify': {
-//        'exports': 'prettyPrintOne'
-//      },
-//      'ng-infinite-scroll': {
-//        'deps': ['angular']
-//      },
-//      '_': {
-//        'exports': '_'
-//      },
-//      'angular-route': {
-//        'deps': ['angular']
-//      },
-//      'ui-router': {
-//        'deps': ['angular']
-//      },
-//      'ui-bootstrap': {
-//        'deps': ['angular']
-//      }
+      //      'angular': {
+      //        'deps': ['jquery'],
+      //        'exports': 'angular'
+      //      },
+      //      'prettify': {
+      //        'exports': 'prettyPrintOne'
+      //      },
+      //      'ng-infinite-scroll': {
+      //        'deps': ['angular']
+      //      },
+      //      '_': {
+      //        'exports': '_'
+      //      },
+      //      'angular-route': {
+      //        'deps': ['angular']
+      //      },
+      //      'ui-router': {
+      //        'deps': ['angular']
+      //      },
+      //      'ui-bootstrap': {
+      //        'deps': ['angular']
+      //      }
     }
   };
 });
