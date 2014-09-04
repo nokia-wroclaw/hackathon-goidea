aca2ed1da97771b1bfafaad96561a008a83d8b22
wincek <marcin.wincek@nsn.com>
2014-09-04 13:15:20 +0200

    Some changes

--- static/js/main.js
+++ static/js/main.js
@@ -2,10 +2,6 @@ require(['static/js/sharedRequireConfig.js'], function(sharedRequireConfig) {
   'use strict';
   require.config(sharedRequireConfig);
 
-  require(['angular', '_'], function(angular, _) {
-
-  require.config(sharedRequireConfig);
-
   require(['angular', 'jquery', 'ui-router', 'bootstrap'], function(angular) {
     console.log(angular);
   });
