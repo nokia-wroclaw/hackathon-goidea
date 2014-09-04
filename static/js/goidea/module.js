0cbe28cfb50bff97623244919c1f16068623211d
chmurson <chmurson@gmail.com>
2014-09-04 14:34:18 +0200

    routing working

--- static/js/goidea/module.js
+++ static/js/goidea/module.js
@@ -1,17 +1,17 @@
 define(['angular',
-//    './namespace',
-//    './controllers/index',
-//    './directives/index',
-//    './services/index',
-    //'./routes/routes'
+    './namespace',
+    './controllers/index',
+    './directives/index',
+    './services/index',
+    './routes/routes'
   ],
   function(ng, namespace) {
     'use strict';
 
     return ng.module(namespace, [
-        //namespace + '.controllers',
-        //namespace + '.directives',
-        //namespace + '.services',
-        //namespace + '.routes'
+        namespace + '.controllers',
+        namespace + '.directives',
+        namespace + '.services',
+        namespace + '.routes'
     ]).run(function() {});
   });
