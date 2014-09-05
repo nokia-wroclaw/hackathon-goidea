eecfb190a898751df392b9dbb40445446637b75a
purplecode <niespammnie@gmail.com>
2014-09-05 15:29:44 +0200

    initial-data fix

--- initial-data.sql
+++ initial-data.sql
@@ -6,10 +6,10 @@ INSERT INTO idea(title, description, creation_date, event_date, status, creator_
 	VALUES ('Obiad dla Chmursona', 'Zamawiaj jedzenie z głodnym Chmursonem', '2012-12-17 13:35:15+01', '2012-12-30 13:35:15+01', 'Proposal', 1,2);
 
 INSERT INTO idea(title, description, creation_date, event_date, status, creator_id, min_assignees)
-	VALUES ('Obiad dla Maki Paki', 'Zamawiaj jedzenie z głodna Maka Paka', '2012-12-18 13:35:15+01', '2012-12-31 13:35:15+01', 'Closed', 0,2);
+	VALUES ('Obiad dla Maki Paki', 'Zamawiaj jedzenie z głodna Maka Paka', '2012-12-18 13:35:15+01', '2012-12-31 13:35:15+01', 'Closed', 2,2);
 
 INSERT INTO idea(title, description, creation_date, event_date,  status, creator_id, min_assignees)
-	VALUES ('Obiad dla Titiferow', 'Zamawiaj jedzenie z głodnymi Titiferami', '2012-12-19 13:35:15+01', '2012-12-31 13:35:15+01', 'Closed', 2,3);
+	VALUES ('Obiad dla Titiferow', 'Zamawiaj jedzenie z głodnymi Titiferami', '2012-12-19 13:35:15+01', '2012-12-31 13:35:15+01', 'Closed', 3,3);
 
 INSERT INTO idea_users(idea_id, user_id)
 	VALUES (1,1);
