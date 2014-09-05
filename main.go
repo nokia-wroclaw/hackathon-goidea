c79cfdd5bd2e13f30cec4149b7ca7ef3a476cb69
purplecode <niespammnie@gmail.com>
2014-09-05 11:22:23 +0200

    database schema creation

--- main.go
+++ main.go
@@ -11,13 +11,18 @@ import (
 
 func init() {
 	orm.RegisterModel(new(models.Idea))
+	orm.RegisterModel(new(models.User))
 	orm.RegisterDataBase("default", "postgres", "user=goidea password=goidea host=localhost port=5432 dbname=goidea sslmode=disable", 30)
 }
 
 func main() {
-//	config := new(modules.Configuration)
-//	auth := new(modules.Authentication)
-//	auth.Login(config.Key("xacas"), "user", "pass");
+	//	config := new(modules.Configuration)
+	//	auth := new(modules.Authentication)
+	//	auth.Login(config.Key("xacas"), "user", "pass");
+
+	// Usage: go run main.go orm syncdb -force=1
+	// orm.RunCommand()
+
 	beego.Run()
 }
 
